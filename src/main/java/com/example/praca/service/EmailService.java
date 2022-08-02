513a8c0e5c745b5be902602b505c99c9cebac706
Szymon Krolik <krolik.sz@wp.pl>
2022-08-02 20:37:42 +0200

    Change confirmationLinks

--- src/main/java/com/example/praca/service/EmailService.java
+++ src/main/java/com/example/praca/service/EmailService.java
@@ -30,7 +30,7 @@ public class EmailService {
 
     protected void sendConfirmationToken(String userMail, String token) {
         final SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        String activationLink = "http://127.0.0.1:8080/user/confirm?token=" + token;
+        String activationLink = "http://146.59.3.226:8080/user/confirm?token=" + token;
         String mailMessage = "Dziękujemy za rejestracje, proszę kliknąć w link aby aktywować konto \n";
         simpleMailMessage.setTo(userMail);
         simpleMailMessage.setSubject("Mail confirmation");
@@ -42,7 +42,7 @@ public class EmailService {
 
     protected void sendConfirmationTokenChangedPassword(String userMail, String token) {
         final SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        String confirmLink = "http://127.0.0.1:8080/user/confirm-password?token=" + token;
+        String confirmLink = "http://146.59.3.226:8080/user/confirm-password?token=" + token;
         String mailMessage = "Hasło do Twojego konta zostało zmienione, proszę potwierdzić zmiany \n";
         simpleMailMessage.setTo(userMail);
         simpleMailMessage.setSubject("Change password confirmation");
@@ -53,7 +53,7 @@ public class EmailService {
 
     protected void sendConfirmationTokenChangedEmail(String userMail, String token) {
         final SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        String confirmLink = "http://127.0.0.1:8080/user/confirm-mail?token=" + token;
+        String confirmLink = "http://146.59.3.226:8080/user/confirm-mail?token=" + token;
         String mailMessage = "Twój email przypisany do konta został zmieniony, proszę potwierdź zmiany \n";
         simpleMailMessage.setTo(userMail);
         simpleMailMessage.setSubject("Change mail confirmation");
